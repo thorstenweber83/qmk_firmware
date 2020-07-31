8db9a1aad780a55b5c89a6da7bee3083f670c698
Thorsten <thorstenweber83@users.noreply.github.com>
2020-08-01 00:23:37 +0200

    EXTRAKEY_ENABLE  = yes

--- keyboards/spaceman/pancake/keymaps/tweber/rules.mk
+++ keyboards/spaceman/pancake/keymaps/tweber/rules.mk
@@ -1,3 +1,4 @@
 BOOTMAGIC_ENABLE  = no	# Virtual DIP switch configuration
 CONSOLE_ENABLE  = no	# Console for debug
 COMMAND_ENABLE  = no    # Commands for debug and configuration
+EXTRAKEY_ENABLE  = yes	# Audio control and System control
